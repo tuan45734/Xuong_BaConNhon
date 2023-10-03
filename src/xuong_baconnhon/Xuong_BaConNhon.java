e96943c4859ee42a0614f3dbb6fd11c14722768e
hiennb67 <hiennbph45717@fpt.edu.vn>
2023-10-03 13:40:35 +0700

    hiennn

--- src/xuong_baconnhon/Xuong_BaConNhon.java
+++ src/xuong_baconnhon/Xuong_BaConNhon.java
@@ -20,6 +20,7 @@ public class Xuong_BaConNhon {
         System.out.println("hi tuoi la tuan");
         System.out.println("hien la toi");
         System.out.println("thu lai phat nua xem duoc khong");
+        System.out.println("lai loi doi anh em oi");
     }
 
     
